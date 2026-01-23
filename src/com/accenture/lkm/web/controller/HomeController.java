cdb30144fd44a6cc7c4237f6a8dd4c8e3187d579
java14070752 <java14070752@example.com>
2026-01-23 10:18:40 +0530

    hehe

--- src/com/accenture/lkm/web/controller/HomeController.java
+++ src/com/accenture/lkm/web/controller/HomeController.java
@@ -4,22 +4,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
+@RestController
 @CrossOrigin(origins="http://localhost:3000")
 public class HomeController {
 
 	private static Logger LOGGER = Logger.getLogger(HomeController.class);
 
-	@RequestMapping(value = "/hello", method = RequestMethod.GET)
-	public ModelAndView showHomePage() {
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	
+	public String showHomePage() {
 		LOGGER.info("Execution Started [showHomePage()]");
 
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("Welcome");
 
 		LOGGER.info("Execution Over [showHomePage()]");
-		return modelAndView;
+		return "Inventory Management System running";
 	}
 }
